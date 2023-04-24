4eab6cfb4c73bfbeadbff77f346d79b6443527cb
lookenghua <lookenghua@gmail.com>
2023-04-24 20:47:35 +0800

    chore: 新增tailwind和naiveui框架

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = process.env.NODE_ENV === 'development'
+
 
 export default defineNuxtConfig({
+    // @ts-ignore
+    components: [
+        '~/components'
+    ],
     modules: [
         '@nuxt/devtools',
-    ]
+        '@huntersofbook/naive-ui-nuxt',
+        '@nuxtjs/tailwindcss',
+    ],
+
+    tailwindcss: {
+        // @ts-ignore
+        corePlugins: {
+            preflight: false,
+        }
+    }
 })
